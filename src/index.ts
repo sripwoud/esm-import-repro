1b95bb0b66a8f0d89aec6e1b24ab989772cb7693
sripwoud <me@sripwoud.xyz>
2024-09-11 13:44:43 +0200

    test importing all modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { derivePublicKey } from "@zk-kit/eddsa-poseidon";
+import { mulPointEscalar } from "@zk-kit/baby-jujub"
+import { IMT } from "@zk-kit/imt"
+import { LazyTowerHashChainProofBuilder } from "@zk-kit/lazytower"
+import { LeanIMT } from "@zk-kit/lean-imt"
+import { tokenize } from "@zk-kit/logical-expressions"
+import { poseidonEncrypt } from "@zk-kit/poseidon-cipher"
+import { generate } from "@zk-kit/poseidon-proof"
+import { SMT } from "@zk-kit/smt"
+import { packing } from "@zk-kit/utils"
 
 const privateKey = "some-private-key";
 
